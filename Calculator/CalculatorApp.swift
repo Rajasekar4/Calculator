efbb981cbfd58e993ea06323733f26a61a5e320b
Rajasekar4 <rajasekar2003@icloud.com>
2024-09-30 00:18:30 +0530

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Raja Sekar.J on 30/09/24.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
